6ffb4be7ebd1d332106ce0ed7694533067cfcbbf
Zyh2333 <1245231959@qq.com>
2020-07-31 19:27:52 +0800

    修改侧边栏bug

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -58,19 +58,6 @@
         
 
       </el-submenu>
-
-      <!-- <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item> -->
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
     </el-menu>
   </el-col>
 </el-row>
